06fdab01a9ad40b404a92b1bc2911d1c90671427
Lucca-Mar <lucca.principal1.0@gmail.com>
2026-09-01 22:14:12 -0300

    aula apache

---
+++ pa-2026/HelloWord/src/helloword/HelloWord.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package helloword;
+
+/**
+ *
+ * @author CAMARGO
+ */
+public class HelloWord 
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {   
+        System.out.println("Hello world");
+       System.out.print("LUCCA Top");
+       System.out.println(1*1);
+       
+        int Numero1 = 10;
+        int Numero2 = 20;
+        int resultado;  
+           
+           
+        // Processamento
+       
+       resultado = Numero1 + Numero2;
+       System.out.println("Esse e o resultado da soma:" + resultado);
+       
+        resultado = Numero1 / Numero2;
+        System.out.println("Esse e o resultado da divisao:" +resultado);
+        
+        resultado = Numero1 * Numero2;
+        System.out.println("Esse e o resultado da multiplicacao:" +resultado);
+        
+        resultado = Numero1 - Numero2;
+        System.out.println("Esse e o resultado da subtracao:" +resultado);
+        
+        //Saida de valores
+        
+        
+        
+        
+    }
+    
+}
---
+++ pa-2026/README.md
@@ -0,0 +1,4 @@
+# PA-2026
+Aula de Programação e Algoritmos
+# Primeira aula de Programação
+ Git HUB
